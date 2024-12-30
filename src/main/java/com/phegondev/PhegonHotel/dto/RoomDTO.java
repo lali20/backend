a6e8b947df57edb275524e53509f8d0a84267599
Sathishsash <sathishsash2003@gmail.com>
2024-12-30 16:05:35 +0530

    updated

---
+++ src/main/java/com/phegondev/PhegonHotel/dto/RoomDTO.java
@@ -0,0 +1,56 @@
+package com.phegondev.PhegonHotel.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class RoomDTO {
+
+    public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getRoomType() {
+		return roomType;
+	}
+	public void setRoomType(String roomType) {
+		this.roomType = roomType;
+	}
+	public BigDecimal getRoomPrice() {
+		return roomPrice;
+	}
+	public void setRoomPrice(BigDecimal roomPrice) {
+		this.roomPrice = roomPrice;
+	}
+	public String getRoomPhotoUrl() {
+		return roomPhotoUrl;
+	}
+	public void setRoomPhotoUrl(String roomPhotoUrl) {
+		this.roomPhotoUrl = roomPhotoUrl;
+	}
+	public String getRoomDescription() {
+		return roomDescription;
+	}
+	public void setRoomDescription(String roomDescription) {
+		this.roomDescription = roomDescription;
+	}
+	public List<BookingDTO> getBookings() {
+		return bookings;
+	}
+	public void setBookings(List<BookingDTO> bookings) {
+		this.bookings = bookings;
+	}
+	private Long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private String roomPhotoUrl;
+    private String roomDescription;
+    private List<BookingDTO> bookings;
+}
---
+++ src/main/java/com/phegondev/PhegonHotel/exception/OurException.java
@@ -0,0 +1,13 @@
+package com.phegondev.PhegonHotel.exception;
+
+public class OurException extends RuntimeException {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public OurException(String message) {
+        super(message);
+    }
+}
